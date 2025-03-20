2942c25f4d15ec9fa52add934ca040ac9b5d99ca
Ali Raza <wirelessvoltswitch@gmail.com>
2025-03-20 17:24:40 +0500

    Enhanced Modularization

--- utilities.py
+++ utilities.py
@@ -1,15 +1,19 @@
 import os
 
-def hide_cursor():
-    """Hides the cursor using ANSI escape sequences."""
-    print("\033[?25l")
-
-
-def show_cursor():
-    """Shows the cursor using ANSI escape sequences."""
-    print("\033[?25h")
-
-
 def clear_screen():
     """Clears the terminal screen."""
     os.system("clear")
+
+
+class CursorRelated:
+    """A class that provides methods for manipulating the cursor."""
+    
+    @staticmethod
+    def hide_cursor():
+        """Hides the cursor using ANSI escape sequences."""
+        print("\033[?25l")
+
+    @staticmethod
+    def show_cursor():
+        """Shows the cursor using ANSI escape sequences."""
+        print("\033[?25h")
